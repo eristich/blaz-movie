a7fd308e5e4e21d655e300e4d915b9400effa6cc
Esteban Ristich <esteban.ristich@protonmail.com>
2023-10-14 06:24:14 +0200

    feat(fixture): add random movies generator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Movie;
+use Faker;
 
 class AppFixtures extends Fixture
 {
+    private Faker\Generator $faker;
+    private ObjectManager $manager;
+    public function __construct() {
+        $this->faker = Faker\Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->manager = $manager;
+        $this->makeMovies(30);
+    }
 
-        $manager->flush();
+    private function makeMovies(int $it = 40): void {
+        for ($i = 0; $i < $it; $i++) {
+            $movie = (new Movie())
+                ->setName($this->faker->company())
+                ->setDescription($this->faker->realText(300, 2))
+                ->setPublicationOn($this->faker->dateTimeBetween());
+            $this->manager->persist($movie);
+        }
+        $this->manager->flush();
     }
 }
