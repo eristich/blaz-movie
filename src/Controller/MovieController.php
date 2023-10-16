7236f488afbcc33a442b527867cfd5688a5176dd
Sachiel <powersosig@riseup.net>
2023-10-16 02:42:30 +0200

    feat(controller): add 'api.movie.create' route

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\Request;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Attributes as OA;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Entity\Movie;
 
+#[Route('/api/v1/movie')]
+#[OA\Tag(name: 'Movie')]
+#[OA\Response(
+    response: 400,
+    description: 'Bad request'
+)]
+#[OA\Response(
+    response: 401,
+    description: 'Unauthorized'
+)]
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
-    public function index(): JsonResponse
+    #[Route('', name: 'api.movie.create', methods: ['POST'])]
+    public function create(
+        Request                 $request,
+        ValidatorInterface      $validator,
+        EntityManagerInterface  $manager,
+        SerializerInterface     $serializer
+    ): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MovieController.php',
-        ]);
+        $movie = $serializer->deserialize($request->getContent(), Movie::class, 'json');
+
+        if ($movie instanceof Movie) {
+            $errors = $validator->validate($movie, null, ['movie:create']);
+            if ($errors->count() > 0) {
+                return new JsonResponse($serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+            }
+            $manager->persist($movie);
+            $manager->flush();
+        }
+
+        return new JsonResponse($serializer->serialize($movie, 'json'), Response::HTTP_CREATED, [], true);
     }
 }
