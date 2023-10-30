ef5432c04da5087bd854b2d2a4ed03b3eb7f3c0a
Esteban Ristich <esteban.ristich@protonmail.com>
2023-10-30 21:29:09 +0100

    feat(repo): add function to filter persons in movie
    Issue #1

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Movie;
+use Doctrine\ORM\Query\Expr;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,6 +21,26 @@ class MovieRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Movie::class);
+    }
+
+    /**
+     * @return QueryBuilder Returns an array of Movie objects
+     */
+    public function findByPersons(array $actorIds = [], array $directorIds = [], string $order = 'DESC'): QueryBuilder
+    {
+        $qb = $this->createQueryBuilder('m');
+        if (!empty($actorIds)) {
+            $qb->innerJoin('m.actors', 'a', Expr\Join::WITH, 'm.id = a.movie');
+            $qb->andWhere('a.person IN (:actorIds)');
+            $qb->setParameter('actorIds', $actorIds);
+        }
+        if (!empty($directorIds)) {
+            $qb->innerJoin('m.directors', 'd', Expr\Join::WITH, 'm.id = d.movie');
+            $qb->andWhere('d.person IN (:directorIds)');
+            $qb->setParameter('directorIds', $directorIds);
+        }
+        $qb->orderBy('m.publication_on', $order);
+        return $qb;
     }
 
 //    /**
